ffa3609b57d3d4bd9fa69b570ac446d92d9fadbb
winnie <2578185702@qq.com>
2022-06-11 16:10:34 +0800

    thrid commit

---
+++ src/main/java/com/example/springboottest/hello/hello2.java
@@ -0,0 +1,17 @@
+package com.example.springboottest.hello;
+
+/**
+ * @author winnie
+ * @PackageName:com.example.springboottest.hello
+ * @Description TODO
+ * @date 2022/6/11 16:09
+ */
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("hello my git1");
+        System.out.println("hello my git2");
+        System.out.println("hello my git3");
+        System.out.println("hello my git4");
+        System.out.println("hello my git5");
+    }
+}
---
+++ src/main/java/com/example/springboottest/hello/hello3.java
@@ -0,0 +1,10 @@
+package com.example.springboottest.hello;
+
+/**
+ * @author winnie
+ * @PackageName:com.example.springboottest.hello
+ * @Description TODO
+ * @date 2022/6/11 15:51
+ */
+public class hello3 {
+}
